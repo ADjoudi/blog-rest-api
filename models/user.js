42ac6783ae812bdc51914c10a7f1104abdf9f2c7
Abdelouahab <98978218+ADjoudi@users.noreply.github.com>
2023-10-10 14:33:43 +0100

    adding local auth

--- models/user.js
+++ models/user.js
@@ -3,11 +3,15 @@ const mongoose = require("mongoose");
 const schema = new mongoose.Schema({
   first_name: { type: String, required: true },
   last_name: { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
   posts: { type: mongoose.Types.Array },
 });
+
+const User = mongoose.model("User", schema);
 
 schema.virtual("full_name").get(function () {
   return `${this.first_name} ${this.last_name}`;
 });
 
-module.exports = mongoose.model("Author", schema);
+module.exports = User;
