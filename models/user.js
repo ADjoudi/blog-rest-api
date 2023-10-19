46da6b90ba94692fc94e77cfa6a739daa973243c
Abdelouahab <98978218+ADjoudi@users.noreply.github.com>
2023-10-19 19:28:38 +0100

    added bookmarks to user model

--- models/user.js
+++ models/user.js
@@ -6,6 +6,7 @@ const schema = new mongoose.Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
   posts: [{ type: mongoose.Types.ObjectId, ref: "Post" }],
+  bookmarks: [{ type: mongoose.Types.ObjectId, ref: "Post" }],
 });
 
 const User = mongoose.model("User", schema);
