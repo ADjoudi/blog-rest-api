33f8ae36c923419fab7e60ca2a4a890e21256792
Abdelouahab <98978218+ADjoudi@users.noreply.github.com>
2023-11-23 12:51:23 +0100

    pending

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const Post = require("../models/post");
+const Comment = require("../models/comment");
 
 //get all posts (sorted by latest)
 router.get("/posts", async function (req, res, next) {
   try {
     const posts = await Post.find().exec();
-    res.json({ posts: posts });
+    res.json(posts);
   } catch (error) {
     res.sendStatus(500);
   }
@@ -14,10 +15,11 @@ router.get("/posts", async function (req, res, next) {
 //get a post and its comments (suggest sign up or login to make a comment)
 router.get("/posts/:post", async function (req, res, next) {
   try {
+    console.log("post id:  ", req.params.post);
     const post = await Post.findById(req.params.post)
-      .populate("Author")
       .populate("Comment")
       .exec();
+    console.log(post);
     res.json({ post: post });
   } catch (error) {
     res.sendStatus(500);
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const Post = require("../models/post");
+const { body, validationResult } = require("express-validator");
 
 router.get("/user", function (req, res, next) {
   const user = {
@@ -16,6 +17,51 @@ router.get("/:user/posts", async function (req, res, next) {
   const posts = await Post.find({ author: req.params.user });
   res.json({ posts: posts });
 });
+
+//make a new post
+router.post("/:user/posts", [
+  function (req, res, next) {
+    if (req.user.id !== req.params.user) {
+      res.sendStatus(400);
+      return;
+    }
+    next();
+  },
+  body("title").trim().escape(),
+  body("content").trim().escape(),
+  body("author").trim().escape(),
+  body("comments").isArray().escape(),
+  body("published").isBoolean().escape(),
+  async function (req, res, next) {
+    const errors = validationResult(req);
+    console.log(req.body);
+    const post = {
+      title: req.body.title,
+      content: req.body.content,
+      author: req.body.author,
+      comments: req.body.comments,
+      published: req.body.published,
+    };
+
+    if (!errors.isEmpty()) {
+      post.errors = errors;
+      res.status(400);
+      res.json(post);
+      return;
+    }
+    try {
+      const newPost = new Post(post);
+      await newPost.save();
+      res.sendStatus(200);
+    } catch (err) {
+      post.errors = err;
+      res.status(500);
+      res.json(post);
+      return;
+    }
+  },
+]);
+
 //get all comments in user's posts
 router.get("/:user/posts", function (req, res, next) {
   res.send("respond with a resource");
@@ -32,10 +78,7 @@ router.get("/:user/posts/published", function (req, res, next) {
 router.get("/:user/posts/unpublished", function (req, res, next) {
   res.send("respond with a resource");
 });
-//make a new post
-router.post("/:user/posts", function (req, res, next) {
-  res.send("respond with a resource");
-});
+
 //preview post
 router.get("/:user/posts/:post", function (req, res, next) {
   res.send("respond with a resource");
